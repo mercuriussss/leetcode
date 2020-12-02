2825602ad0b1e166223a80e381d478b99840f8c5
jianlong <caijianlong1@globalegrow.com>
2020-12-02 18:36:14 +0800

    第4题(中等)、第19题(困难)

--- src/com/cjl/sword/Solution_4.java
+++ src/com/cjl/sword/Solution_4.java
@@ -22,4 +22,65 @@ package com.cjl.sword;
 
 public class Solution_4 {
 
+    // 时间复杂度为O(M+N)，M和N为矩阵的行列数，空间复杂度为O（1）
+    public boolean solution1(int[][] matrix, int target) {
+        int col = 0;
+        int row = matrix.length - 1;
+        while(row >= 0 && col < matrix[0].length){
+            if(matrix[row][col] > target){
+                row --;
+            }else if(matrix[row][col] < target){
+                col ++;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    // 二分查找法，时间复杂度为是O(klg(h)) k=min(m,n) h=max(m,n)，空间复杂度为O(1)
+    // (评论区的做法，不确定时间复杂度是否正确，实际上跑起来比方法1要费时间，似乎找不到时间复杂度为O(n)以下的解法)
+    public boolean solution2(int[][] matrix, int target) {
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0){
+            return false;
+        }
+        int row = matrix.length;
+        int col = matrix[0].length;
+        if(row > col){
+            for (int i = 0; i < col; i++) {
+                int[] tmp = new int[row];
+                for (int j = 0; j < row; j++) {
+                    tmp[j] = matrix[j][i];
+                }
+                if(binarySearch(target,tmp)){
+                    return true;
+                }
+            }
+        }else{
+            for (int i = 0; i < row; i++) {
+                if(binarySearch(target,matrix[i])){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean binarySearch(int target,int[] arr){
+        int low = 0;
+        int high = arr.length - 1;
+        int mid;
+        while(low <= high){
+            mid = (low + high)/2;
+            if(target == arr[mid]){
+                return true;
+            }else if(target > arr[mid]){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+        return false;
+    }
 }
