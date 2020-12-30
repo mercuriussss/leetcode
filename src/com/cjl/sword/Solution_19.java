67c373d325edff745518cc92dfec6a23b410945a
jianlong <caijianlong1@globalegrow.com>
2020-12-30 18:38:55 +0800

    19题改进，20题（中等），21（未完成）

--- src/com/cjl/sword/Solution_19.java
+++ src/com/cjl/sword/Solution_19.java
@@ -74,9 +74,9 @@ public class Solution_19 {
     }
 
 
-    public boolean solution2(String A,String B){
-        int n = A.length();
-        int m = B.length();
+    public boolean solution2(String str,String reg){
+        int n = str.length();
+        int m = reg.length();
         boolean[][] f = new boolean[n + 1][m + 1];
 
         for (int i = 0; i <= n; i++) {
@@ -86,8 +86,8 @@ public class Solution_19 {
                     f[i][j] = i == 0;
                 } else {
                     //非空正则分为两种情况 * 和 非*
-                    if (B.charAt(j - 1) != '*') {
-                        if (i > 0 && (A.charAt(i - 1) == B.charAt(j - 1) || B.charAt(j - 1) == '.')) {
+                    if (reg.charAt(j - 1) != '*') {
+                        if (i > 0 && (str.charAt(i - 1) == reg.charAt(j - 1) || reg.charAt(j - 1) == '.')) {
                             f[i][j] = f[i - 1][j - 1];
                         }
                     } else {
@@ -97,7 +97,7 @@ public class Solution_19 {
                             f[i][j] |= f[i][j - 2];
                         }
                         //看
-                        if (i >= 1 && j >= 2 && (A.charAt(i - 1) == B.charAt(j - 2) || B.charAt(j - 2) == '.')) {
+                        if (i >= 1 && j >= 2 && (str.charAt(i - 1) == reg.charAt(j - 2) || reg.charAt(j - 2) == '.')) {
                             f[i][j] |= f[i - 1][j];
                         }
                     }
