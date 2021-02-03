4212e53b60f6b5731af1a1ccec55c6d002e4ab3f
jianlong <caijianlong1@globalegrow.com>
2021-02-03 18:26:34 +0800

    53,54

--- src/com/cjl/sword/Solution_53_I.java
+++ src/com/cjl/sword/Solution_53_I.java
@@ -24,8 +24,11 @@ public class Solution_53_I {
         int i = 0, j = nums.length - 1;
         while(i <= j) {
             int m = (i + j) >>> 1;
-            if(nums[m] <= tar) i = m + 1;
-            else j = m - 1;
+            if(nums[m] <= tar) {
+                i = m + 1;
+            } else {
+                j = m - 1;
+            }
         }
         return i;
     }
