c792f889a4f0ef6bc498b994730368bfd4290c48
jianlong <caijianlong1@globalegrow.com>
2021-02-19 18:39:48 +0800

    新增2题,已刷79题

---
+++ src/com/cjl/interview/Choice_Top_Interview_3.java
@@ -0,0 +1,47 @@
+package com.cjl.interview;
+
+import java.util.*;
+
+/*
+    问题描述：
+        给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+    示例 1:
+        输入: s = "abcabcbb"
+        输出: 3
+        解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+    示例 2:
+        输入: s = "bbbbb"
+        输出: 1
+        解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+    示例 3:
+        输入: s = "pwwkew"
+        输出: 3
+        解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+             请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+    示例 4:
+        输入: s = ""
+        输出: 0
+    提示：
+        0 <= s.length <= 5 * 104
+        s 由英文字母、数字、符号和空格组成
+ */
+public class Choice_Top_Interview_3 {
+
+    // 时间复杂度是O(N)，空间复杂度是O(N)
+    public static int solution1(String s) {
+        if(s.length() == 0){
+            return 0;
+        }
+        int res = 0;
+        int left = 0;
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                left = Math.max(map.get(s.charAt(i)), left);
+            }
+            res = Math.max(res, i - left + 1);
+            map.put(s.charAt(i), i + 1);
+        }
+        return res;
+    }
+}
