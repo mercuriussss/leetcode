6ff8f0124d64887b284a8176d0936fd1c5693317
mercuriussss <caijianlong615@outlook.com>
2021-06-22 00:26:21 +0800

    重新开始，题目只按leetcode原排序做归类，已做3道题

---
+++ src/com/cjl/old/sword/Sword_40.java
@@ -0,0 +1,92 @@
+package com.cjl.old.sword;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/*
+    问题描述：
+        输入整数数组 arr ，找出其中最小的 k 个数。
+        例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。
+    示例 1：
+        输入：arr = [3,2,1], k = 2
+        输出：[1,2] 或者 [2,1]
+    示例 2：
+        输入：arr = [0,1,2,1], k = 1
+        输出：[0]
+    限制：
+        0 <= k <= arr.length <= 10000
+        0 <= arr[i]<= 10000
+ */
+public class Sword_40 {
+
+    // 快排变形
+    // 时间复杂度O(N)，空间复杂度O(1)
+    public int[] solution1(int[] arr, int k) {
+        if (arr == null || k == 0 || arr.length == 0) {
+            return new int[0];
+        }
+        // 最后一个参数表示我们要找的是下标为k-1的数
+        return quickSearch(arr, 0, arr.length - 1, k - 1);
+    }
+
+    private int[] quickSearch(int[] nums, int lo, int hi, int k) {
+        // 每快排切分1次，找到排序后下标为j的元素，如果j恰好等于k就返回j以及j左边所有的数；
+        int j = partition(nums, lo, hi);
+        if (j == k) {
+            return Arrays.copyOf(nums, j + 1);
+        }
+        // 否则根据下标j与k的大小关系来决定继续切分左段还是右段。
+        return j > k ? quickSearch(nums, lo, j - 1, k) : quickSearch(nums, j + 1, hi, k);
+    }
+
+    // 快排切分，返回下标j，使得比nums[j]小的数都在j的左边，比nums[j]大的数都在j的右边。
+    private int partition(int[] nums, int lo, int hi) {
+        int v = nums[lo];
+        int i = lo, j = hi + 1;
+        while (true) {
+            while (++i <= hi && nums[i] < v) ;
+            while (--j >= lo && nums[j] > v) ;
+            if (i >= j) {
+                break;
+            }
+            swap(nums, i, j);
+        }
+        swap(nums, lo, j);
+        return j;
+    }
+
+    private void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    // 堆排序
+    // 时间复杂度O(nlogk)，空间复杂度O(k)
+    public int[] solution2(int[] arr, int k) {
+        if (k == 0) {
+            return new int[0];
+        }
+        // 使用一个最大堆（大顶堆）
+        // Java 的 PriorityQueue 默认是小顶堆，添加 comparator 参数使其变成最大堆
+        Queue<Integer> heap = new PriorityQueue<>(k, (i1, i2) -> i2 - i1);
+
+        for (int e : arr) {
+            // 当前数字小于堆顶元素才会入堆
+            if (heap.isEmpty() || heap.size() < k || e < heap.peek()) {
+                heap.offer(e);
+            }
+            if (heap.size() > k) {
+                heap.poll(); // 删除堆顶最大元素
+            }
+        }
+        // 将堆中的元素存入数组
+        int[] res = new int[heap.size()];
+        int j = 0;
+        for (int e : heap) {
+            res[j++] = e;
+        }
+        return res;
+    }
+}
