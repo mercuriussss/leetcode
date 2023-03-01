96e0cc37e1e1a583e3f139643ba5b5fc30986a24
ouroboros <caijianlong615@outlook.com>
2023-03-01 14:18:06 +0800

    fix range

--- src/com/cjl/leetcode/Question_200.java
+++ src/com/cjl/leetcode/Question_200.java
@@ -45,7 +45,7 @@ public class Question_200 {
     }
 
     private void markRecur(char[][] grid, int row, int col) {
-        if(row < 0 || col < 0 || row > grid.length || col > grid[0].length || grid[row][col] != '1') {
+        if(row < 0 || col < 0 || row >= grid.length || col >= grid[0].length || grid[row][col] != '1') {
             return;
         }
         grid[row][col] = '2';
