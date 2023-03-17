0b535086c1bea0fded71b23db00331217defbb87
ouroboros <caijianlong615@outlook.com>
2023-03-17 11:57:29 +0800

    新增2题，累积50题

---
+++ src/com/cjl/leetcode/Question_104.java
@@ -0,0 +1,37 @@
+package com.cjl.leetcode;
+
+import com.cjl.common.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+    104. 二叉树的最大深度
+    问题描述：
+        给定一个二叉树，找出其最大深度。
+        二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+        说明: 叶子节点是指没有子节点的节点。
+    示例：
+        给定二叉树 [3,9,20,null,null,15,7]，
+
+            3
+           / \
+          9  20
+            /  \
+           15   7
+        返回它的最大深度 3 。
+ */
+public class Question_104 {
+
+    // 时间复杂度是O(N)，空间复杂度是O(1)
+    public int solution1(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    private int dfs(TreeNode root, int level) {
+        if(root == null) {
+            return level;
+        }
+        return Math.max(dfs(root.left, level + 1), dfs(root.right, level + 1));
+    }
+}
